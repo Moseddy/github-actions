acb956f60acfb7a7c25be23a50b32e22bbe1dbf1
Moseddy <moseddy1@gmail.com>
2025-07-03 19:24:11 +0100

    made some comments

--- src/main/java/com/fullStack/employee/EmployeeApplication.java
+++ src/main/java/com/fullStack/employee/EmployeeApplication.java
@@ -6,14 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeApplication {
 
-//	echo "# github-actions" >> README.md
-//	git init
-//	git add README.md
-//	git commit -m "first commit"
-//	git branch -M main
-//	git remote add origin https://github.com/Moseddy/github-actions.git
-//	git push -u origin main
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
